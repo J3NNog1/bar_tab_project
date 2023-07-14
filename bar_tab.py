f784a8d8761a8120b03c3bf166003bc1747dc16e
jennifer j <jenniferjuarezjj@gmail.com>
2023-07-14 18:10:00 -0400

    create menu item dictionary

---
+++ bar_tab.py
@@ -0,0 +1,11 @@
+class Tab:
+
+    menu = {
+        'wine': 5,
+        'beer': 3, 
+        'soft_drink': 2,
+        'chicken': 10, 
+        'beef' : 15,
+        'veggie' : 12,
+        'desert' : 6
+    }
